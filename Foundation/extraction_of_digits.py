96def444ffe7a786605ecd772851ac2acf5517f7
rahul-gitt <postbox9635@gmail.com>
2026-08-13 00:59:26 +0530

    Explain a logics and Mention Time complexity and Space complexity

--- Foundation/extraction_of_digits.py
+++ Foundation/extraction_of_digits.py
@@ -5,3 +5,18 @@ while num > 0:
     last_digit = num % 10
     print("The last digit is :",last_digit)
     num = num// 10
+
+# Logic : 
+'''
+n= 1234
+num = n
+while num > 0:
+    last = num % 10         -> 1234 % 10 = 4 
+    print(last)             -> print = 4
+    num = num // 10         -> 123
+    and goes on upto num = 0
+    then loop break
+
+Time complexity = O(n)
+
+'''
